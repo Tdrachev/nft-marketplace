89752ca6f24e4b0b48b9954f0b8e60fb0f7d0668
Tdrachev <drachev.td@gmail.com>
2021-12-09 15:07:32 +0200

    Added canceling and completing a sale

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract NFTMarketplace is ReentrancyGuard {
 	using Counters for Counters.Counter;
 	Counters.Counter private _itemID;
-	Counters.Counter private _listingID;
 	Counters.Counter private _itemsSold;
 	Counters.Counter private _collectionID;
 
@@ -22,17 +21,17 @@ contract NFTMarketplace is ReentrancyGuard {
 		address payable seller;
 		address payable owner;
 		bool sold;
+		Listing[] listingHistory;
+		Purchase[] purchaseHistory;
 	}
 
 	struct Purchase {
-		uint256 purchaseID;
 		uint256 price;
 		address seller;
 		address buyer;
 	}
 
 	struct Listing {
-		uint256 listingID;
 		uint256 price;
 		address seller;
 		bool canceled;
@@ -55,9 +54,8 @@ contract NFTMarketplace is ReentrancyGuard {
 	address payable marketplaceOwner;
 	uint256 marketplaceFee = 0.025 ether;
 
+	mapping(uint256 => uint256) tokenIdToId;
 	mapping(uint256 => MarketItem) idToMarketItem;
-	mapping(uint256 => Purchase[]) idToPurchaseHistory;
-	mapping(uint256 => Listing[]) idToListingHistory;
 	mapping(uint256 => Collection) idToCollection;
 
 	event MarketItemListed(
@@ -126,8 +124,16 @@ contract NFTMarketplace is ReentrancyGuard {
 			"Marketplace Fee not covered, please try again"
 		);
 
+		payable(address(this)).transfer(msg.value);
+
 		_itemID.increment();
 		uint256 newItemID = _itemID.current();
+
+		Listing[] memory newListings = new Listing[](1);
+		newListings[1] = Listing(price, msg.sender, false);
+
+		Purchase[] memory purchases = new Purchase[](1);
+
 		idToMarketItem[newItemID] = MarketItem(
 			newItemID,
 			tokenID,
@@ -135,28 +141,17 @@ contract NFTMarketplace is ReentrancyGuard {
 			nftContract,
 			payable(msg.sender),
 			payable(address(0)),
-			false
+			false,
+			newListings,
+			purchases
 		);
+		tokenIdToId[tokenID] = newItemID;
 		IERC721(nftContract).approve(address(this), tokenID);
 		IERC721(nftContract).safeTransferFrom(
 			msg.sender,
 			address(this),
 			tokenID
 		);
-
-		_listingID.increment();
-		uint256 newListingID = _listingID.current();
-
-		Listing[] memory oldListings = idToListingHistory[newItemID];
-		Listing[] memory newListings = new Listing[](oldListings.length + 1);
-		newListings[oldListings.length] = Listing(
-			newListingID,
-			price,
-			msg.sender,
-			false
-		);
-
-		idToListingHistory[newItemID] = newListings;
 
 		emit MarketItemListed(
 			tokenID,
@@ -165,5 +160,89 @@ contract NFTMarketplace is ReentrancyGuard {
 			msg.sender,
 			address(0)
 		);
+	}
+
+	function cancelMarketListing(uint256 tokenID) public payable nonReentrant {
+		uint256 itemID = tokenIdToId[tokenID];
+		MarketItem memory marketItemToCancel = idToMarketItem[itemID];
+		address nftContract = marketItemToCancel.nftContract;
+
+		marketItemToCancel.sold = false;
+		marketItemToCancel.owner = payable(msg.sender);
+
+		Listing[] memory listingHistory = marketItemToCancel.listingHistory;
+
+		listingHistory[listingHistory.length].canceled = true;
+
+		idToMarketItem[itemID] = marketItemToCancel;
+
+		IERC721(nftContract).safeTransferFrom(
+			address(this),
+			marketItemToCancel.seller,
+			marketItemToCancel.tokenID
+		);
+
+		emit MarketItemCanceled(tokenID, nftContract, msg.sender, msg.sender);
+	}
+
+	function completeMarketListing(uint256 tokenID)
+		public
+		payable
+		nonReentrant
+	{
+		uint256 itemID = tokenIdToId[tokenID];
+		MarketItem memory toBeSold = idToMarketItem[itemID];
+
+		require(
+			msg.value == toBeSold.price,
+			"The item you are trying to buy costs more than you are trying to send"
+		);
+		marketplaceOwner.transfer(marketplaceFee);
+		toBeSold.seller.transfer(msg.value);
+
+		IERC721(toBeSold.nftContract).safeTransferFrom(
+			address(this),
+			msg.sender,
+			tokenID
+		);
+
+		toBeSold.sold = true;
+		toBeSold.seller = payable(msg.sender);
+		toBeSold.owner = payable(msg.sender);
+
+		Listing[] memory listingHistory = toBeSold.listingHistory;
+		Listing memory toBeCanceled = listingHistory[listingHistory.length - 1];
+
+		toBeCanceled.canceled = true;
+
+		listingHistory[listingHistory.length - 1] = toBeCanceled;
+		toBeSold.listingHistory = listingHistory;
+
+		idToMarketItem[itemID] = toBeSold;
+		_itemsSold.increment();
+		emit MarketItemSold(
+			tokenID,
+			msg.value,
+			toBeSold.nftContract,
+			toBeSold.seller,
+			msg.sender,
+			true
+		);
+	}
+
+	function getAllListedItems() public view returns (MarketItem[] memory) {
+		uint256 totalListed = _itemID.current();
+		uint256 totalNotSold = totalListed - _itemsSold.current();
+		uint256 toBeReturnedIndex = 0;
+		MarketItem[] memory toBeReturned = new MarketItem[](totalNotSold);
+
+		for (uint256 i = 0; i < totalNotSold; i++) {
+			if (idToMarketItem[i].owner == address(0)) {
+				toBeReturned[toBeReturnedIndex] = idToMarketItem[i];
+				toBeReturnedIndex++;
+			}
+		}
+
+		return toBeReturned;
 	}
 }
